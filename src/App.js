642dc854b754867938e455518c955f51a3520067
iddimsangi <adammsangi@gmail.com>
2022-01-12 08:00:22 +0300

    folder structure and layout

--- src/App.js
+++ src/App.js
@@ -1,22 +1,15 @@
-import logo from './img/logo.svg';
+import { useState } from 'react'
 import './App.scss';
-
+import Form from './components/Form/Form'
 function App() {
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <h1>Todo App</h1>
+      <div className="App-header-box">
+          <Form/>
+      </div>
       </header>
     </div>
   );
---
+++ src/components/Form/Form.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import './Form.scss'
+function Form() {
+    return (
+        <div className="form-box">
+            <h1>hello form</h1>
+        </div>
+    )
+}
+
+export default Form
